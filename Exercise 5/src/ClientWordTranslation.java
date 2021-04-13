466a2ff98107c200985c22676cacd6ad774f3a1c
Asyraf391 <asyrafhanif391@gmail.com>
2021-04-13 09:51:26 +0800

    Exercise 5 : Word translation

---
+++ Exercise 5/src/ClientWordTranslation.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
+
+public class ClientWordTranslation {
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+			// Connect to the server at localhost. port 4230
+			Socket socket = new Socket (InetAddress.getLocalHost(), 4233);
+			
+			// Create input stream
+			BufferedReader bufferedReader = new BufferedReader( new InputStreamReader (socket.getInputStream()));
+			
+			String text = bufferedReader.readLine();
+			System.out.println(text);
+			
+		
+			// Close everything
+			bufferedReader.close();
+			socket.close();
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
